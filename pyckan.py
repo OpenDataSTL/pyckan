356ca79d43e9390b727de7b4aee83666cce89c8f
Nacho <iggy.soriano@gmail.com>
2018-08-11 13:50:17 -0500

    package_patch methods

--- pyckan.py
+++ pyckan.py
@@ -15,9 +15,24 @@ def run():
     groups = demo.action.package_list(id='test_data')
     print(groups)
 
-    pkg = demo.action.package_create(name='my-dataset', title='not going to work')
+    try:
+        pkg = demo.action.package_create(name='my-dataset', title='not going to work')
+    except:
+        print("create_error")
+
+    pkg = demo.action.package_show(id='my-dataset')
+
     groups = demo.action.package_list(id='test_data')
     print(groups)
+    print("\nPackage_Create {}".format(pkg))
+
+    pkg['title'] = "WORKING!!"
+    pkg['tags'] = ['a_tag', 'b_tag']
+    pkg['description'] = 'Just playing around really...'
+
+    print("\nPackage_Create {}".format(pkg))
+
+    pkg = demo.action.package_patch(id='my-dataset',  data_dict=pkg)
 
 if __name__ == "__main__":
     run()
